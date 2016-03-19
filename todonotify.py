06952de9c7f1a7cbab579adeaec99e95128e505d
shekhar-singh <sschoudhary60@gmail.com>
2016-03-19 06:33:29 +0530

    added try-except handler

--- todonotify.py
+++ todonotify.py
@@ -1,11 +1,14 @@
 from datetime import datetime, time
 from time import sleep
 import os
+try:
+    f=open("file.txt")
+    x=f.readlines()
+    for i in x:
+        os.system("notify-send %s" %i)
+        sleep(360)
+except IOError:
+   print "Error: can\'t find file or read data"
 
-f=open("file.txt")
-x=f.readlines()
-for i in x:
-    os.system("notify-send %s" %i)
-    sleep(360)
 
 
